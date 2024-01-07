f295951f816e95e198d7dd913a00bbb8287fde67
Yulia <sivonenkou@gmail.com>
2024-01-07 20:26:40 +0200

    add

---
+++ 01-creation.js
@@ -0,0 +1,17 @@
+const playlist = {
+    name: 'my superhero',
+    rating: 5,
+    tracks: ['track-1', 'track-2', 'track-3'],
+    treckCount: 3,
+}
+console.log(playlist);
+// console.log(playlist.tracks);
+// console.log(playlist.name);
+// console.log(playlist.treckCount);
+
+const propertyName = 'tracks'
+// console.log(playlist.rating);
+// console.log(playlist['rating']);
+// console.log(playlist.propertyName);
+// console.log(propertyName.propertyName);
+// console.log(playlist[propertyName]);
