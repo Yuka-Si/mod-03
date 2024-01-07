f2d96294ea7eee4e4598e40436a1b6806936af1d
Yulia <sivonenkou@gmail.com>
2024-01-07 22:08:25 +0200

    card

---
+++ 04-card.js
@@ -0,0 +1,14 @@
+const cart = {
+    items: [],
+    getItems() { },
+    add(product) { },
+    remove(productName) { },
+    clear() { },
+    countTotalPrice() { },
+    increaseQuantity(productName) { },
+    decreaseQuantity(productName) { },
+};
+cart.add({ name: '🍎', price: 50 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍓', price: 110 });
