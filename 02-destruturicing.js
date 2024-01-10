ec72a945cd60a95b311c6a63813bc6af6d5c8696
Yulia <148382792+Yuka-Si@users.noreply.github.com>
2024-01-11 00:29:14 +0200

    add

--- 02-destruturicing.js
+++ 02-destruturicing.js
@@ -1,14 +1,33 @@
-const profile = {
-    name: 'Jacques Gluke',
-    tag: 'jgluke',
-    location: 'Ocho Rios, Jamaica',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-    stats: {
-        followers: 5603,
-        views: 4827,
-        likes: 1308,
-    },
-};
+// const profile = {
+//     name: 'Jacques Gluke',
+//     tag: 'jgluke',
+//     location: 'Ocho Rios, Jamaica',
+//     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//     stats: {
+//         followers: 5603,
+//         views: 4827,
+//         likes: 1308,
+//     },
+// };
 
-const { name, tag, location, avatar, stats } = profile;
-console.log(name, tag, location, avatar, followers, views, likes);
+// const { name, tag, location, avatar, stats } = profile;
+// console.log(name, tag, location, avatar, followers, views, likes);
+
+
+const rgb = [255, 100, 80];
+const [red, green, blue] = rgb;
+// console.log(red, green, blue);
+
+
+const authors = {
+    kiwi: 4,
+    poly: 7,
+    ajax: 9,
+    mango: 6,
+};
+const entries = Object.entries(authors);
+
+for (const [name, rating] of entries) {
+
+    console.log(name, rating);
+}
