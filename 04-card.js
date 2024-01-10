643ffd639ccebabdfca5ce831359a79d17d79d15
Yulia <148382792+Yuka-Si@users.noreply.github.com>
2024-01-10 23:23:19 +0200

    add

--- 04-card.js
+++ 04-card.js
@@ -8,6 +8,9 @@ const cart = {
     increaseQuantity(productName) { },
     decreaseQuantity(productName) { },
 };
+
+
+
 cart.add({ name: '🍎', price: 50 });
 cart.add({ name: '🍋', price: 60 });
 cart.add({ name: '🍋', price: 60 });
